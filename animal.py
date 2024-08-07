0c158cd819cac9f95429214ff7224863ad8faf1f
Oleksandr Semenenko <115368382+sashasemenenko@users.noreply.github.com>
2024-08-07 14:28:16 +0200

    zoo project

---
+++ animal.py
@@ -0,0 +1,15 @@
+class Animal:
+    def __init__(self, name: str, age: int):
+        self._name = name
+        self._age = age
+
+    def eat(self):
+        print(f"{self._name} is eating")
+
+    def sleep(self):
+        print(f"{self._name} is sleeping")
+
+    def make_sound(self):
+        pass
+
+
